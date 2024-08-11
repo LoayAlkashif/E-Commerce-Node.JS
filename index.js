f7d744bf88d80085d2eeba13fb1901c8c067e493
LoayAlkashif <loayalkashif@gmail.com>
2024-08-11 15:38:44 +0300

    new order

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ import dotenv from "dotenv";
 import { catchError } from "./src/middleware/catchError.js";
 
 import Stripe from "stripe";
+import { Order } from "./database/models/order.model.js";
+import { User } from "./database/models/user.model.js";
+import { Cart } from "./database/models/cart.model.js";
+import { Product } from "./database/models/product.model.js";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 dotenv.config();
@@ -18,7 +22,7 @@ const port = process.env.PORT || 3000;
 app.post(
   "/api/webhook",
   express.raw({ type: "application/json" }),
-  catchError((req, res) => {
+  catchError(async (req, res, next) => {
     const sig = req.headers["stripe-signature"].toString();
 
     let event = stripe.webhooks.constructEvent(
@@ -30,6 +34,35 @@ app.post(
     let checkout;
     if (event.type == "checkout.session.completed") {
       checkout = event.data.object;
+
+      let user = await User.findOne({ email: checkout.customer_email });
+      let cart = await Cart.findById(checkout.client_reference_id);
+      if (!cart) return next(new AppError("cart not found", 404));
+
+      let order = new Order({
+        user: req.user._id,
+        orderItems: cart.cartItems,
+        shippingAddress: checkout.metadata,
+        totalOrderPrice: checkout.amount_total / 100,
+        paymentType: "card",
+        isPaid: true,
+      });
+      await order.save();
+
+      // increment sold & decrement stock to product
+      let options = cart.cartItems.map((product) => {
+        return {
+          updateOne: {
+            filter: { _id: product.product },
+            update: {
+              $inc: { sold: product.quantity, stock: -product.quantity },
+            },
+          },
+        };
+      });
+      await Product.bulkWrite(options);
+      // clear user cart
+      await Cart.findByIdAndDelete(cart._id);
     }
 
     // Return a 200 res to acknowledge receipt of the event

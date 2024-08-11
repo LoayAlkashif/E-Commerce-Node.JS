24518b8018d85db29caf50992f0abeb6e5fc160a
LoayAlkashif <loayalkashif@gmail.com>
2024-08-11 14:12:11 +0300

    stripe added

--- index.js
+++ index.js
@@ -5,11 +5,38 @@ import { bootstrap } from "./src/bootstrap.js";
 import { dbConnection } from "./database/dbConnection.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import { catchError } from "./src/middleware/catchError.js";
+
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.post(
+  "/api/webhook",
+  express.raw({ type: "application/json" }),
+  catchError((req, res) => {
+    const sig = req.headers["stripe-signature"].toString();
+
+    let event = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      "whsec_ZL8b0G0NwJD86SdB52zotki89WUrlQh3"
+    );
+
+    let checkout;
+    if (event.type == "checkout.session.completed") {
+      checkout = event.data.object;
+    }
+
+    // Return a 200 res to acknowledge receipt of the event
+    res.json({ message: "Success", checkout });
+  })
+);
+
 app.use(cors());
 app.use(express.json());
 
